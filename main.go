c0839dec9fafbd165b3ed4f81ba8a0ca8088e794
Murat Mert Guler <m.guler@gardiyan.com>
2025-03-19 16:10:01 +0300

    editted congfig.go for windows and main.go

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/adaptor"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"go.uber.org/zap"
 	"microservicetest/pkg/config"
 	_ "microservicetest/pkg/log"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -21,7 +24,35 @@ func main() {
 
 	zap.L().Info("starting server...")
 
-	app := fiber.New()
+	httpClient :=  &http.Client{
+		Transport: &http.Transport{
+			Dial: (&net.Dialer{
+				Timeout:   5 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).Dial,
+			TLSHandshakeTimeout:   5 * time.Second,
+			ResponseHeaderTimeout: 5 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+		
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://www.google.com", nil)
+	if err != nil {
+		zap.L().Error("failed to create request", zap.Error(err)
+	)
+
+
+
+	app := fiber.New(fiber.Config{
+		IdleTimeout:  5 * time.Second,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+		Concurrency:  256 * 1024,
+	})
+
 	app.Get("/metrics", adaptor.HTTPHandler(promhttp.Handler()))
 	app.Get("/", func(c *fiber.Ctx) error {
 		zap.L().Info("server started")
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+
 	"github.com/spf13/viper"
 )
 
@@ -10,12 +11,13 @@ type AppConfig struct {
 }
 
 func Read() *AppConfig {
-	viper.SetConfigName("config")      // name of config file (without extension)
-	viper.SetConfigType("yaml")        // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("$PWD/config") // call multiple times to add many search paths
-	viper.AddConfigPath(".")           // optionally look for config in the working directory
-	err := viper.ReadInConfig()        // Find and read the config file
-	if err != nil {                    // Handle errors reading the config file
+	viper.SetConfigName("config")                               // name of config file (without extension)
+	viper.SetConfigType("yaml")                                 // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("$PWD/config")                          // call multiple times to add many search paths
+	viper.AddConfigPath("C:\\projects\\microservice\\config\\") // call multiple times to add many search paths
+	viper.AddConfigPath(".")                                    // optionally look for config in the working directory
+	err := viper.ReadInConfig()                                 // Find and read the config file
+	if err != nil {                                             // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
 
